8f89460235913ffc65eed8685c55c1844077d07c
shiho-aoki <s.aoki.ase@gmail.com>
2021-04-09 09:39:35 +0000

    [ADD] and gate use wight and bias

--- perceptron.cpp
+++ perceptron.cpp
@@ -6,6 +6,8 @@ using namespace std;
 int andgate(int x1, int x2){
     float tmp, w1=0.5, w2=0.5, theta=0.7;
     tmp = x1*w1 + x2*w2;
+    cout << "and gate" << endl;
+    cout << tmp << endl;
     if (tmp<= theta){
         return 0;
     }else if(tmp > theta){
@@ -22,14 +24,20 @@ int nadngate(int x1, int x2){
 
 int andgate_use_wAndbias(int x1, int x2){
     int x[2] = {x1,x2};
-    float tmp=0.0, w[2] = {0.5, 0.5}, b = -0.7;
-    for (int i=0; i++; i<3){
-        tmp += x[i]* w[i] + b;
-        cout << tmp << endl;
+    float tmp, w[2] = {0.5, 0.5}, b =0.7;
+    for(int i=1; i<3; i++){
+        cout << "i"<< endl;
+        cout << i << endl;
     }
+    float data1, data2;
+    data1 = x[1]*w[1];
+    data2 = x[2]*w[2];
+    tmp = data1 + data2;
+    cout << "and w and b gate" << endl;
+    cout << tmp << endl;
     if (tmp<=0){
         return 0;
-    }else{
+    }else if(tmp>0){
         return 1;
     }
     return 0;
