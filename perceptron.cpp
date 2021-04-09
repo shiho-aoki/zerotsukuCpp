41b05ec908ef7bf9d7891fe6e3d36e6c53049076
shiho-aoki <s.aoki.ase@gmail.com>
2021-04-09 12:08:32 +0000

    [ADD] NAND, OR Gate

--- perceptron.cpp
+++ perceptron.cpp
@@ -14,24 +14,52 @@ int andgate(int x1, int x2){
     return 0;
 };
 
-int nadngate(int x1, int x2){
-    int x[2] = { x1,x2 };
-    float w[2] = { -0.5, -0.5 };
-    return 0;
-}
-
 int andgate_use_wAndbias(int x1, int x2){
     int x[2] = {x1,x2};
-    float tmp, w[2] = {0.5, 0.5}, b =0.7;
+    float tmp, w[2] = {0.5, 0.5}, b =-0.7;
     int *xp = x;
     float *wp=w;
     for(int i=0; i<2; i++){
         tmp += *(xp+i) * *(wp+i);
     }
-    tmp -= b;
+    tmp += b;
     if (tmp<=0){
         return 0;
     }else if(tmp>0){
+        return 1;
+    }
+    return 0;
+}
+
+int nadngate_use_wAndbias(int x1, int x2){
+    int x[2] = { x1,x2 };
+    float tmp, w[2] = { -0.5, -0.5 }, b=0.7;
+    int *xp = x;
+    float *wp = w;
+    for (int i=0; i<2; i++){
+        tmp += *(xp+i) * *(wp+i);
+    }
+    tmp += b;
+    if(tmp <=0){
+        return 0;
+    }else{
+        return 1;
+    }
+    return 0;
+}
+
+int orgate_use_wAndbias(int x1, int x2){
+    int x[2] = { x1, x2 };
+    float tmp, w[2]={ 0.5, 0.5 }, b=-0.2;
+    int *xp = x;
+    float *wp = w;
+    for (int i=0; i<2; i++){
+        tmp += *(xp+i) * *(wp+i);
+    }
+    tmp += b;
+    if (tmp<=0){
+        return 0;
+    }else{
         return 1;
     }
     return 0;
