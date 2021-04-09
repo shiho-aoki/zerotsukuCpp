e47f022c66e0952da744f52be3d041b1c22bfa09
shiho-aoki <s.aoki.ase@gmail.com>
2021-04-09 11:51:23 +0000

    [FIX] and gate use POINTER2

--- perceptron.cpp
+++ perceptron.cpp
@@ -6,8 +6,6 @@ using namespace std;
 int andgate(int x1, int x2){
     float tmp, w1=0.5, w2=0.5, theta=0.7;
     tmp = x1*w1 + x2*w2;
-    cout << "and gate" << endl;
-    cout << tmp << endl;
     if (tmp<= theta){
         return 0;
     }else if(tmp > theta){
@@ -25,16 +23,12 @@ int nadngate(int x1, int x2){
 int andgate_use_wAndbias(int x1, int x2){
     int x[2] = {x1,x2};
     float tmp, w[2] = {0.5, 0.5}, b =0.7;
-    for(int i=1; i<3; i++){
-        cout << "i"<< endl;
-        cout << i << endl;
+    int *xp = x;
+    float *wp=w;
+    for(int i=0; i<2; i++){
+        tmp += *(xp+i) * *(wp+i);
     }
-    float data1, data2;
-    data1 = x[1]*w[1];
-    data2 = x[2]*w[2];
-    tmp = data1 + data2;
-    cout << "and w and b gate" << endl;
-    cout << tmp << endl;
+    tmp -= b;
     if (tmp<=0){
         return 0;
     }else if(tmp>0){
