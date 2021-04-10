4b4e17453f94397aa94e5cac143eece83249f453
shiho-aoki <s.aoki.ase@gmail.com>
2021-04-10 07:57:48 +0000

    [ADD] multi Layter Perceptron

---
+++ multiLayeredPerceptron.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+
+using namespace std;
+
+int andgate();
+int andgate_use_wAndbias();
+int nandgate_use_wAndbias();
+int orgate_use_wAndbias();
+
+// int xorgate(int x1, int x2){
+
+// }
